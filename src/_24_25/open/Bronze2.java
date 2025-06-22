a9daa0e6f99ac85dc3109fd74a0239b6bbae5498
Shaoyu Wang <shaoyuwang119@gmail.com>
2025-06-22 16:27:16 -0500

    added Bronze2 and Bronze3

--- src/_24_25/open/Bronze2.java
+++ src/_24_25/open/Bronze2.java
@@ -3,6 +3,43 @@ import java.util.*;
 
 public class Bronze2 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int cases = scan.nextInt();
+        scan.nextLine();
+        for (int c=0; c<cases; c++) {
+            int cows = scan.nextInt();
+            int photoCows = 0;
+            scan.nextLine();
+            HashMap<Integer, Integer> heights = new HashMap<>();
+            int largest = 0;
+
+            for (int i=0; i<cows; i++) {
+                int height = scan.nextInt();
+                if (!heights.containsKey(height)) {
+                    heights.put(height, 0);
+                }
+                heights.put(height, heights.get(height)+1);
+                if (height > largest) {
+                    largest = height;
+                }
+
+            }
+            //System.out.println("largest " + largest);
+            // set the value of the largest key to 0
+            heights.put(largest, 0);
+            photoCows++;
+            //System.out.println(heights);
+
+            for (int cow : heights.keySet()) {
+                if (heights.get(cow) > 1) {
+                    photoCows += 2;
+                }
+            }
+
+            System.out.println(photoCows);
+            scan.nextLine();
+        }
+
 
     }
 }
---
+++ src/_24_25/open/Bronze3.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Bronze3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int stringLen = scan.nextInt();
+        int queries = scan.nextInt();
+        scan.nextLine();
+        String string = scan.nextLine();
+        long start = System.currentTimeMillis();
+        char[] stringArr = string.toCharArray();
+
+        for (; queries>0; queries--) {
+            int value = -1;
+            int left = scan.nextInt();
+            int right = scan.nextInt();
+
+            System.out.println(findLargestValue(left-1, right-1, stringArr));
+        }
+        //System.out.println(System.currentTimeMillis() - start + "ms");
+
+    }
+
+    static long findLargestValue(int left, int right, char[] arr) {
+        long value = -1;
+        int i;
+        int k;
+
+        for (i = left; i < right-2; i++) {
+            //System.out.println("i = " + i);
+            double iMax = Math.pow((double) (right-i)/2, 2);
+            if (iMax < (double) value) {
+                break;
+            }
+
+            for (k = right; k > i+1; k--) {
+                double kMax = Math.pow((double) (k-i)/2, 2);
+                if (kMax < (double) value) {
+                    //System.out.println(k);
+                    //System.out.println("asdfgfdsa");
+                    break;
+                }
+
+                if (arr[k] == arr[i]) {
+                    //System.out.println(left + " " + right + " eqas");
+                    continue;
+                }
+
+                int j = (k+i)/2;
+                //System.out.println(j);
+                int jL = j;
+                int jR = j+1;
+                for (; jL > i; jL--) {
+                    if (arr[jL] == arr[k]) {
+                        //System.out.println("break!L " + jL + " " + j);
+                        break;
+                    }
+                }
+                for (; jR < k; jR++) {
+                    if (arr[jR] == arr[k]) {
+                        //System.out.println("break!R " + jR + " " + j);
+                        break;
+                    }
+                }
+                if (jL == i && jR == k) continue;
+
+                if (j-jL < jR-j) {
+                    j = jL;
+                } else {
+                    j = jR;
+                }
+
+                long newValue = (long) (k - j) * (j - i);
+                if (newValue > value) {
+                    value = newValue;
+                    if (value == 0) {
+                        //System.out.println(i+" "+jL+" "+k);
+                    }
+                    //System.out.println(i + " " + j + " " + k + " " + jL + " " + jR + " " + value);
+                }
+            }
+
+        }
+
+        return value;
+
+    }
+}
